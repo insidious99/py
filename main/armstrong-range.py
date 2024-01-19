3f205478210c06bba05346612781cbd3fbedd84d
insidious99 <143763951+insidious99@users.noreply.github.com>
2024-01-19 12:34:32 +0530

    Create armstrong-range.py

---
+++ main/armstrong-range.py
@@ -0,0 +1,13 @@
+# Armstrong number in given range
+for i in range(1, 10000):
+    sum = 0
+    temp = i
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** 3
+        temp //= 10
+
+    if i == sum:
+        print(i, "is an Armstrong number")
+    else:
+        continue
